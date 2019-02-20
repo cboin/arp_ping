823ccddba03e7f5cc9414a6677f2f1bddf275c49
Clément Boin <clem.boin@gmail.com>
2019-02-20 22:27:56 +0000

    define arp packet as struct

--- arp_ping.c
+++ arp_ping.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
+struct arp_hdr
+{
+	uint16_t ah_htype;
+	uint16_t ah_ptype;
+	uint8_t ah_hlen;
+	uint8_t ah_plen;
+	uint8_t ah_oper;
+	uint8_t ah_sha[6];
+	uint8_t ah_spa[4];
+	uint8_t ah_tha[6];
+	uint8_t ah_tpa[4];
+} __attribute__ ((packed));
 
 int main(void)
 {
-	printf("Hello, world!\n");
-
 	return EXIT_SUCCESS;
 }
