de4c2cb106be26709ecd790bbe555b4e631e2624
Clément Boin <clem.boin@gmail.com>
2019-03-30 21:24:54 +0100

    search for given interface

--- arp_ping.c
+++ arp_ping.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <linux/if.h>
+#include <string.h>
 
 static void usage(const int status)
 {
@@ -10,6 +14,35 @@ options:\n\
 	-h, --help	display this help and exit\n");
 
 	exit(status);
+}
+
+/* get_iface_info: get ip and mac address from the given interface. */
+void get_iface_info(const char *iface_name)
+{
+	struct ifaddrs *addrs, *next;
+	unsigned int iface_found;
+
+	getifaddrs(&addrs);
+	iface_found = 0;
+
+	next = addrs;
+	while (next) {
+		if (strncmp(next->ifa_name, iface_name, IFNAMSIZ) == 0) {
+			iface_found = 1;	
+			break;
+		}
+
+		next = next->ifa_next;
+	}
+
+	freeifaddrs(addrs);
+
+	if (!iface_found) {
+		fprintf(stderr, "error: interface %s not found\n", iface_name);
+		exit(EXIT_FAILURE);
+	}
+
+	return NULL;
 }
 
 int main(int argc, char **argv)
@@ -26,7 +59,7 @@ int main(int argc, char **argv)
 		getopt_long(argc, argv, "i:", long_options, NULL)) != -1) {
 		switch (opt) {
 		case 'i':
-			/* TODO: get ip and hardware address of given iface. */
+			get_iface_info(optarg);
 			break;
 		case 'h':
 			usage(EXIT_SUCCESS);
